faf0b20aa1dca9ea4497a43d720d477411529764
Benjamin Cole <benjamin.cole@kent-202-15m.local>
2016-01-22 15:23:27 +1300

    use AJAX to connect to server

--- js/ajax-yoobee.js
+++ js/ajax-yoobee.js
@@ -1,1 +1,26 @@
-alert('hi');
+$(document).ready(function(){
+
+	//Listen to the form
+	$('#note-form').submit(function(e){
+
+		//Prevent form from submitting
+		e.preventDefault();
+
+		//Grab the note
+		if ($.trim( $('#note').val() ) == '') {
+			return;
+		}
+
+		//Prepare AJAX
+		$.ajax({
+			url: 'http://bentley.cole.yoobee.net.nz/ajax-yoobee/ajax-yoobee.php',
+			data: {note: $('#note').val()},
+			success: function(dataFromServer) {
+
+			},
+			error: function() {
+				alert('Cannot Connect');
+			}
+		});
+	});
+});
